fa51c839de2e9964099cf523615855ecacbeb440
v4t <39200058+v4t@users.noreply.github.com>
2019-02-04 23:10:11 +0200

    Start working on expression parsing

--- src/main/scala/minipl/Parser.scala
+++ src/main/scala/minipl/Parser.scala
@@ -12,9 +12,21 @@ case class VariableDeclaration(name: String, varType: String) extends Statement
 
 case class VariableAssignment(name: String, value: Statement) extends Statement
 
-case class VariableDeclarationWithAssignment(name: String, varType: String, value: Statement) extends Statement
+case class VariableDeclarationWithAssignment(name: String, varType: String, value: Expression) extends Statement
 
-case class Expression() extends Statement
+sealed trait Expression
+
+case class UnaryNot(expr: Expression) extends Expression
+
+case class ArithmeticExpression(leftHand: Expression, op: String, rightHand: Expression) extends Expression
+
+case class BooleanExpression(leftHand: Expression, op: String, rightHand: Expression) extends Expression
+
+case class IntLiteral(value: Int) extends Expression
+
+case class StringLiteral(value: String) extends Expression
+
+case class VariableRef(value: String) extends Expression
 
 object Parser extends RegexParsers {
 
@@ -37,45 +49,56 @@ object Parser extends RegexParsers {
   def statement: Parser[Statement] = (declaration ||| declarationWithAssignment ||| readOp) <~ ";"
 
   def declaration: Parser[Statement] =
-    "var" ~> varIdent ~ ":" ~ varType ^^ {
-      case vName ~ _ ~ vType => VariableDeclaration(vName, vType)
+    "var" ~> varRef ~ ":" ~ varType ^^ {
+      case VariableRef(vName) ~ _ ~ vType => VariableDeclaration(vName, vType)
     }
 
   def declarationWithAssignment: Parser[Statement] =
-    "var" ~> varIdent ~ ":" ~ varType ~ ":=" ~ expr ^^ {
-      case vName ~ _ ~ vType ~ _ ~ value => VariableDeclarationWithAssignment(vName, vType, NoOp())
+    "var" ~> varRef ~ ":" ~ varType ~ ":=" ~ expr ^^ {
+      case VariableRef(vName) ~ _ ~ vType ~ _ ~ vValue => VariableDeclarationWithAssignment(vName, vType, vValue)
     }
 
-  def expr: Parser[Statement] =
-    (varIdent ||| stringLiteral ||| intLiteral ||| binaryExpr ||| unaryExpr ||| subExpr) ^^ {
-      _ => NoOp()
-    }
+  def expr: Parser[Expression] = operand ||| unaryNot ||| binaryExpr
 
-  def binaryExpr: Parser[Statement] = "!" ~> expr ^^ {
-    _ => NoOp()
+  def binaryExpr: Parser[Expression] = arithmeticExpr ||| booleanExpr
+
+  def arithmeticExpr: Parser[Expression] = operand ~ """[\+\-\*\/]""".r ~ operand ^^ {
+    case lhs ~ op ~ rhs => ArithmeticExpression(lhs, op, rhs)
   }
 
-  def unaryExpr: Parser[Statement] = "!" ~> expr ^^ {
-    _ => NoOp()
+  def booleanExpr: Parser[Expression] = operand ~ """[\&\=\<]""".r ~ operand ^^ {
+    case lhs ~ op ~ rhs => BooleanExpression(lhs, op, rhs)
   }
 
-  def subExpr: Parser[Statement] = "(" ~> expr <~ ")" ^^ {
-    _ => NoOp()
+  def unaryNot: Parser[Expression] = "!" ~> expr ^^ {
+    expr => UnaryNot(expr)
   }
+
+  def subExpr: Parser[Expression] = "(" ~> expr <~ ")"
+
+  def operand: Parser[Expression] = varRef ||| stringLiteral ||| intLiteral ||| subExpr
 
   def varType: Parser[String] = """(int|bool|string)""".r
 
-  def varIdent: Parser[String] = """[A-Za-z_][a-zA-Z0-9]*""".r
+  def varRef: Parser[VariableRef] =
+    """[A-Za-z_][a-zA-Z0-9]*""".r ^^ {
+      name => VariableRef(name)
+    }
 
-  def intLiteral: Parser[Int] = """[1-9][0-9]*""".r ^^ (i => i.toInt)
+  def intLiteral: Parser[IntLiteral] =
+    """[1-9][0-9]*""".r ^^ (i => i.toInt) ^^ {
+      value => IntLiteral(value)
+    }
 
-  def stringLiteral: Parser[String] = """\".*\"""".r
+  def stringLiteral: Parser[StringLiteral] =
+    """\".*\"""".r ^^ {
+      value => StringLiteral(value)
+    }
 
-  def readOp: Parser[Statement] = "read " ~ varIdent ^^ (_ => NoOp())
+  def readOp: Parser[Statement] = "read " ~ varRef ^^ (_ => NoOp())
 
   def printOp: Parser[Statement] = "print " ~ expr ^^ (_ => NoOp())
 
   def assertOp: Parser[Statement] = "assert " ~ expr ^^ (_ => NoOp())
-
 
 }
