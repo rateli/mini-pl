7327e60257280fd0945fc108f0634cc74eaca967
v4t <39200058+v4t@users.noreply.github.com>
2019-02-14 23:24:20 +0200

    Interpret assertion and print statements

--- src/main/scala/minipl/Interpreter.scala
+++ src/main/scala/minipl/Interpreter.scala
@@ -1,5 +1,57 @@
 package minipl
 
+import minipl.TypeChecker.SymbolTable
+
+import scala.annotation.tailrec
+
+sealed trait Value
+
+final case class IntValue(value: Int) extends Value
+
+final case class StringValue(value: String) extends Value
+
+final case class BoolValue(value: Boolean) extends Value
+
 object Interpreter {
 
+  def runProgram(program: List[Statement], symbolTable: SymbolTable): SymbolTable = {
+    visit(program, symbolTable)
+  }
+
+  @tailrec
+  def visit(program: List[Statement], symbolTbl: SymbolTable): SymbolTable = program match {
+    case Nil => symbolTbl
+    case stmt :: rest => visit(rest, visit(stmt, symbolTbl))
+  }
+
+  def visit(stmt: Statement, symbolTbl: SymbolTable): SymbolTable = stmt match {
+    case s@VariableDeclaration(_, _, _) => visit(s, symbolTbl)
+    case s@VariableAssignment(_, _) => visit(s, symbolTbl)
+    case s@ForLoop(_, _, _, _) => visit(s, symbolTbl)
+    case s@ReadOp(_) => visit(s, symbolTbl)
+    case s@PrintOp(_) => visit(s, symbolTbl)
+    case s@AssertOp(_) => visit(s, symbolTbl)
+  }
+
+  def visit(assertOp: AssertOp, symbolTable: SymbolTable): SymbolTable = {
+    visit(assertOp.expr, symbolTable) match {
+      case BoolValue(result) => if (!result) throw MiniPLAssertionError() else symbolTable
+    }
+  }
+
+  def visit(printOp: PrintOp, symbolTable: SymbolTable): SymbolTable = {
+    visit(printOp.value, symbolTable) match {
+      case StringValue(result) => println(result)
+        symbolTable
+    }
+  }
+
+  def visit(expr: Expression, symbolTbl: SymbolTable): Value = expr match {
+    case StringLiteral(value) => StringValue(value)
+    case IntLiteral(value) => IntValue(value)
+    case v@VariableRef(_) => visit(v, symbolTbl)
+    case e@UnaryNot(_) => visit(e, symbolTbl)
+    case e@ArithmeticExpression(_, _, _) => visit(e, symbolTbl)
+    case e@BooleanExpression(_, _, _) => visit(e, symbolTbl)
+  }
 }
--- src/main/scala/minipl/exceptions.scala
+++ src/main/scala/minipl/exceptions.scala
@@ -3,3 +3,5 @@ package minipl
 final case class MiniPLSyntaxError(msg: String) extends Exception
 
 final case class MiniPLSemanticError(msg: String) extends Exception
+
+final case class MiniPLAssertionError() extends Exception
