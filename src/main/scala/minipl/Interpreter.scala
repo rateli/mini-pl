81ba28d438cdbfca51a18bdf5f9ba21971c1b49b
v4t <39200058+v4t@users.noreply.github.com>
2019-02-14 23:42:47 +0200

    Add value table to interpreter

--- src/main/scala/minipl/Interpreter.scala
+++ src/main/scala/minipl/Interpreter.scala
@@ -14,44 +14,46 @@ final case class BoolValue(value: Boolean) extends Value
 
 object Interpreter {
 
+  type ValueTable = Map[String, Value]
+
   def runProgram(program: List[Statement], symbolTable: SymbolTable): SymbolTable = {
-    visit(program, symbolTable)
+    visit(program, symbolTable, Map.empty)
   }
 
   @tailrec
-  def visit(program: List[Statement], symbolTbl: SymbolTable): SymbolTable = program match {
+  def visit(program: List[Statement], symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = program match {
     case Nil => symbolTbl
-    case stmt :: rest => visit(rest, visit(stmt, symbolTbl))
+    case stmt :: rest => visit(rest, visit(stmt, symbolTbl, valueTbl), valueTbl)
   }
 
-  def visit(stmt: Statement, symbolTbl: SymbolTable): SymbolTable = stmt match {
-    case s@VariableDeclaration(_, _, _) => visit(s, symbolTbl)
-    case s@VariableAssignment(_, _) => visit(s, symbolTbl)
-    case s@ForLoop(_, _, _, _) => visit(s, symbolTbl)
-    case s@ReadOp(_) => visit(s, symbolTbl)
-    case s@PrintOp(_) => visit(s, symbolTbl)
-    case s@AssertOp(_) => visit(s, symbolTbl)
+  def visit(stmt: Statement, symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = stmt match {
+    case s@VariableDeclaration(_, _, _) => visit(s, symbolTbl, valueTbl)
+    case s@VariableAssignment(_, _) => visit(s, symbolTbl, valueTbl)
+    case s@ForLoop(_, _, _, _) => visit(s, symbolTbl, valueTbl)
+    case s@ReadOp(_) => visit(s, symbolTbl, valueTbl)
+    case s@PrintOp(_) => visit(s, symbolTbl, valueTbl)
+    case s@AssertOp(_) => visit(s, symbolTbl, valueTbl)
   }
 
-  def visit(assertOp: AssertOp, symbolTable: SymbolTable): SymbolTable = {
-    visit(assertOp.expr, symbolTable) match {
-      case BoolValue(result) => if (!result) throw MiniPLAssertionError() else symbolTable
+  def visit(assertOp: AssertOp, symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = {
+    visit(assertOp.expr, symbolTbl, valueTbl) match {
+      case BoolValue(result) => if (!result) throw MiniPLAssertionError() else symbolTbl
     }
   }
 
-  def visit(printOp: PrintOp, symbolTable: SymbolTable): SymbolTable = {
-    visit(printOp.value, symbolTable) match {
+  def visit(printOp: PrintOp, symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = {
+    visit(printOp.value, symbolTbl, valueTbl) match {
       case StringValue(result) => println(result)
-        symbolTable
+        symbolTbl
     }
   }
 
-  def visit(expr: Expression, symbolTbl: SymbolTable): Value = expr match {
+  def visit(expr: Expression, symbolTbl: SymbolTable, valueTbl: ValueTable): Value = expr match {
     case StringLiteral(value) => StringValue(value)
     case IntLiteral(value) => IntValue(value)
-    case v@VariableRef(_) => visit(v, symbolTbl)
-    case e@UnaryNot(_) => visit(e, symbolTbl)
-    case e@ArithmeticExpression(_, _, _) => visit(e, symbolTbl)
-    case e@BooleanExpression(_, _, _) => visit(e, symbolTbl)
+    case v@VariableRef(_) => visit(v, symbolTbl, valueTbl)
+    case e@UnaryNot(_) => visit(e, symbolTbl, valueTbl)
+    case e@ArithmeticExpression(_, _, _) => visit(e, symbolTbl, valueTbl)
+    case e@BooleanExpression(_, _, _) => visit(e, symbolTbl, valueTbl)
   }
 }
