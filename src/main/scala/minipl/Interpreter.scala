417c9c529edb1d5f638befc0cc5acdffd474823d
v4t <39200058+v4t@users.noreply.github.com>
2019-02-19 21:31:32 +0200

    Work on evaluating binary operator expressions

--- src/main/scala/minipl/Interpreter.scala
+++ src/main/scala/minipl/Interpreter.scala
@@ -17,13 +17,13 @@ object Interpreter {
   type ValueTable = Map[String, Value]
 
   def runProgram(program: List[Statement], symbolTable: SymbolTable): SymbolTable = {
-    visit(program, symbolTable, Map.empty)
+    run(program, symbolTable, Map.empty)
   }
 
   @tailrec
-  def visit(program: List[Statement], symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = program match {
+  def run(program: List[Statement], symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = program match {
     case Nil => symbolTbl
-    case stmt :: rest => visit(rest, visit(stmt, symbolTbl, valueTbl), valueTbl)
+    case stmt :: rest => run(rest, visit(stmt, symbolTbl, valueTbl), valueTbl)
   }
 
   def visit(stmt: Statement, symbolTbl: SymbolTable, valueTbl: ValueTable): SymbolTable = stmt match {
@@ -68,5 +68,101 @@ object Interpreter {
     visit(not.expr, symbolTbl, valueTbl) match {
       case BoolValue(result) => BoolValue(!result)
     }
+
+  def visit(expr: ArithmeticExpression, symbolTbl: SymbolTable, valueTbl: ValueTable): Value = {
+    val leftHand = visit(expr.leftHand, symbolTbl, valueTbl)
+    val rightHand = visit(expr.rightHand, symbolTbl, valueTbl)
+    expr.op match {
+      case Plus() => plus(leftHand, rightHand)
+      case Minus() => minus(leftHand, rightHand)
+      case Mul() => mul(leftHand, rightHand)
+      case Div() => div(leftHand, rightHand)
+    }
+  }
+
+  def plus(lhs: Value, rhs: Value): Value = {
+    if(lhs.isInstanceOf[StringType] || rhs.isInstanceOf[StringType]) concat(lhs, rhs)
+    val leftHand = lhs match {
+      case IntValue(value) => value
+    }
+    val rightHand = rhs match {
+      case IntValue(value) => value
+    }
+    IntValue(leftHand + rightHand)
+  }
+
+  def concat(lhs: Value, rhs: Value): StringValue = {
+    val leftHand = lhs match {
+      case StringValue(value) => value
+      case IntValue(value) => value.toString
+      case BoolValue(value) => value.toString
+    }
+    val rightHand = rhs match {
+      case StringValue(value) => value
+      case IntValue(value) => value.toString
+      case BoolValue(value) => value.toString
+    }
+    StringValue(leftHand + rightHand)
+  }
+
+  def minus(lhs: Value, rhs: Value): IntValue = {
+    val leftHand = lhs match {
+      case IntValue(value) => value
+    }
+    val rightHand = rhs match {
+      case IntValue(value) => value
+    }
+    IntValue(leftHand - rightHand)
+  }
+
+  def mul(lhs: Value, rhs: Value): IntValue = {
+    val leftHand = lhs match {
+      case IntValue(value) => value
+    }
+    val rightHand = rhs match {
+      case IntValue(value) => value
+    }
+    IntValue(leftHand * rightHand)
+  }
+
+  def div(lhs: Value, rhs: Value): IntValue = {
+    val leftHand = lhs match {
+      case IntValue(value) => value
+    }
+    val rightHand = rhs match {
+      case IntValue(value) => value
+    }
+    IntValue(leftHand / rightHand)
+  }
+
+  def visit(expr: BooleanExpression, symbolTbl: SymbolTable, valueTbl: ValueTable): BoolValue = {
+    val leftHand = visit(expr.leftHand, symbolTbl, valueTbl)
+    val rightHand = visit(expr.rightHand, symbolTbl, valueTbl)
+    expr.op match {
+      case Eq() => BoolValue(leftHand == rightHand)
+      case And() => and(leftHand, rightHand)
+      case LT() => lessThan(leftHand, rightHand)
+    }
+  }
+
+  def and(lhs: Value, rhs: Value): BoolValue = {
+    val leftHand = lhs match {
+      case BoolValue(value) => value
+    }
+    val rightHand = rhs match {
+      case BoolValue(value) => value
+    }
+    BoolValue(leftHand && rightHand)
+  }
+
+  def lessThan(lhs: Value, rhs: Value): BoolValue = {
+    val leftHand = lhs match {
+      case IntValue(value) => value
+    }
+    val rightHand = rhs match {
+      case IntValue(value) => value
+    }
+    BoolValue(leftHand < rightHand)
+  }
 
 }
